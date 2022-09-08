415c6d4ec47d6487d26d2b8d04a117036ed0e98f
Miranda Holmlund <mirreanda@hotmail.com>
2022-09-08 14:38:18 +0200

    Small randomized character working.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { CharacterRaces } from "./src/character-races.js"
 import { FantasyClasses } from "./src/fantasy-classes.js"
+import { CharacterGenerator } from "./src/character-generator.js"
 
 const characterRaces = new CharacterRaces()
 const fantasyClasses = new FantasyClasses()
+const character = new CharacterGenerator()
 const race = characterRaces.getRandomRace()
 const fantasyClass = fantasyClasses.getRandomFantasyClass()
+character.generateCharacter("Cow")
 
 console.log(race)
 console.log(fantasyClass)
+console.log(character.getCharacterStats())
---
+++ src/character-generator.js
@@ -0,0 +1,48 @@
+import { CharacterRaces } from "./character-races.js";
+import { FantasyClasses } from "./fantasy-classes.js";
+
+export class CharacterGenerator {
+    #characterRaces
+    #fantasyClasses
+    constructor() {
+        this.#characterRaces = new CharacterRaces()
+        this.#fantasyClasses = new FantasyClasses()
+        this.name
+        this.race
+        this.charClass
+    }
+
+    setName(name) {
+        if(name === undefined) {
+            this.name = "Unknown name"
+        } else {
+            this.name = name
+        }
+    }
+
+    setRace(race) {
+        if(race === undefined) {
+            this.race = this.#characterRaces.getRandomRace()
+        } else {
+            this.race = race
+        }
+    }
+
+    setClass(charClass) {
+        if(charClass === undefined) {
+            this.charClass = this.#fantasyClasses.getRandomFantasyClass()
+        } else {
+            this.charClass = charClass
+        }
+    }
+
+    generateCharacter(name, race, charClass) {
+        this.setName(name)
+        this.setRace(race)
+        this.setClass(charClass)
+    }
+
+    getCharacterStats() {
+        return {"Name" : this.name, "Race" : this.race, "Class" : this.charClass}
+    }
+}
