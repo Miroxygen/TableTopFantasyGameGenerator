17e937994b974def5ae40bd1f634527afb5a43bf
Miranda Holmlund <mirreanda@hotmail.com>
2022-09-09 12:37:21 +0200

    Random value for resources tested

--- test/character-races.test.js
+++ test/character-races.test.js
@@ -2,8 +2,13 @@ import { CharacterRaces } from "../src/character-races.js";
 
 const characterRaces = new CharacterRaces()
 
-test('Class can give a randomized race', () => {
+test('Class can give a randomized race thats part of the races', () => {
     const races = characterRaces.getAllRaces()
-    const randomRace = characterRaces.getRandomRace()
-    expect(races.includes(randomRace)).toEqual(true)
+    const firstRandomRace = characterRaces.getRandomRace()
+    expect(races.includes(firstRandomRace)).toEqual(true)
+    const secondRandomRace = characterRaces.getRandomRace()
+    /*There is a chance the two different rolls will roll the same value, (1/9).
+    So this test will eventually fail even though it does roll a random value.
+    I still wanted to test that it indeed can roll two different values 8 out of 9 times.*/ 
+    expect(firstRandomRace).not.toEqual(secondRandomRace)
 })
