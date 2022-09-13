922c159bbffd85bc5f12f035ec3269ab1dff4a97
Miranda Holmlund <mirreanda@hotmail.com>
2022-09-13 16:57:58 +0200

    Card flip and info added

--- src/html-components/random-character-card.js
+++ src/html-components/random-character-card.js
@@ -3,8 +3,31 @@ import { CharacterGenerator } from "../generators/character-generator.js"
 const template = document.createElement('template')
 template.innerHTML = `
 <style>
+#card {
+    height:600px;
+    width:550px;
+    margin:0 auto;
+}
+#cardIcon {
+    border-radius:50%;
+    height:300px;
+    position:absolute;
+    width:250px;
+}
+#cardInfo {
+    border:solid black;
+    height:600px;
+    width:550px;
+    position:absolute;
+}
+.hidden {
+    display:none;
+}
 </style>
 <div id="card">
+<img id="cardIcon" src="../../images/sorceress.jpg">
+<div id="cardInfo" class="hidden">
+</div>
 </div>
 `
 
@@ -12,13 +35,32 @@ customElements.define('random-character-card',
 
 class extends HTMLElement {
     #card
+    #cardIcon
+    #cardInfo
     constructor() {
         super()
         this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
 
         this.#card = this.shadowRoot.querySelector('#card')
+        this.#cardIcon = this.shadowRoot.querySelector('#cardIcon')
+        this.#cardInfo = this.shadowRoot.querySelector('#cardInfo')
         this.characterGenerator = new CharacterGenerator()
+        this.characterGenerator.generateCharacter('Alina')
+        this.characterInfo = this.characterGenerator.getCharacterStats()
+        this.#card.addEventListener('click', (event) => {
+             this.flipCharacterCard()
+             this.displayCardInfo()
+        })
     }
-}
-)
+
+    flipCharacterCard() {
+        this.#cardIcon.classList.toggle('hidden')
+        this.#cardInfo.classList.toggle('hidden')
+    }
+
+    displayCardInfo() {
+        const characterInfo = this.characterGenerator.getCharacterStats()
+        this.#cardInfo.textContent = `Name : ${characterInfo.Name}  Class : ${characterInfo.Class} \n Race : ${characterInfo.Race}`
+    }
+})
